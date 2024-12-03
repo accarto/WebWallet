29f7a814df65ab24f504ff2b9975c555699ba722
Duddino <duddino@duddino.com>
2024-12-03 10:28:02 +0100

    Use MPW-playback submodule instead of storing the files directly (#479)
    * Use MPW-playback submodule instead of storing the files directly
    
    * Try using https?
    
    * Submodule
    
    * Enable lfs
    
    * Update e2e playbacks
    
    * Empty commit
    
    * Fix e2e test for real this time
    
    * Use different chain params for e2e testing
    
    * Oh wow, it now works. I can't believe it. It will never break again
    
    * Finally works gg

--- cypress/e2e/private_mode.cy.js
+++ cypress/e2e/private_mode.cy.js
@@ -1,34 +1,38 @@
 describe('public/private mode tests', () => {
-    beforeEach(() => {
+    const before = () => {
+        cy.playback('GET', /(xpub|address|getshielddata|duddino|block)/, {
+            matching: { ignores: ['hostname', 'port'] },
+        }).as('sync');
+        cy.intercept('GET', 'https://rpc.duddino.com/mainnet/getblockcount', {
+            statusCode: 200,
+            body: 4669846,
+        });
         cy.clearDb();
         cy.visit('/');
         cy.waitForLoading().should('be.visible');
-        cy.playback('GET', /(xpub|address|getshielddata)/, {
-            matching: { ignores: ['hostname', 'port'] },
-        }).as('sync');
-        cy.setExplorer(0);
-        cy.setNode(1);
+
         cy.goToTab('dashboard');
         cy.importWallet(
             'hawk crash art bottom rookie surprise grit giant fitness entire course spray'
         );
         cy.encryptWallet('123456');
-
         cy.waitForSync();
         cy.togglePrivateMode();
-    });
+    };
 
     it('switches back to public mode when not available', () => {
+        // It should remember private mode
+        before();
+        cy.visit('/');
+        cy.waitForSync();
+
+        cy.get('[data-testid="shieldModePrefix"]').should('exist');
         // We should be in private mode here
         cy.get('[data-testid="shieldModePrefix"]').should('exist');
         cy.deleteWallet();
         // When importing a non shield capable wallet, we should be in public mode
         cy.importWallet('DLabsktzGMnsK5K9uRTMCF6NoYNY6ET4Bb');
+        cy.waitForSync();
         cy.get('[data-testid="shieldModePrefix"]').should('not.exist');
-    });
-
-    it('remembers private mode', () => {
-        cy.visit('/');
-        cy.get('[data-testid="shieldModePrefix"]').should('exist');
     });
 });
